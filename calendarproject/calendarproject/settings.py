7f7b30f869ab7006fc2f82db804022c486a89039
Cho-SangHyun <98shcho@naver.com>
2021-08-12 21:44:43 +0900

    add gitignore

--- calendarproject/calendarproject/settings.py
+++ calendarproject/calendarproject/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path, os
-import dj_database_url
-
-db_from_env=dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +150,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
